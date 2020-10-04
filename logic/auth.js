c016ba31b630882fa0d1df9dec39f79e54b3efcb
lordjoo <=>
2020-10-04 19:17:21 +0200

    Push Manage Users Files

--- logic/auth.js
+++ logic/auth.js
@@ -1,21 +1,28 @@
 $(document).ready(function () {
-    $("#register_btn").click(function () {
+    $("#register").submit(function (e) {
+        e.preventDefault()
         let email = $("#register_email").val();
         let password = $("#register_password").val();
         let name = $("#register_name").val();
-        
-        // TODO: Validate if the inputs are empty 
+        if(password.length < 8){
+            $("#error-box").text("Password must bo longer than 8 Chars").show();
+            return;
+        }
+        $("#error-box").hide();
 
-        firebase.auth().createUserWithEmailAndPassword(email, password).then(data => {
-            console.log(data.user);
-            // TODO redirect back to home
-
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(async res => {
+            // TODO: Let user know that the request has been sent by any method 
+            
+            let user = res.user;
+            // console.log(user.providerData[0])
+            await user.updateProfile({displayName:name});
         }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log(errorCode)
             console.log(errorMessage)
+            $("#error-box").text(errorMessage).show()
           });
 
     })
